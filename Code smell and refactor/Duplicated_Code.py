842a37bc74cd358bd4e070c1506568e86f8bac5b
namnx228 <namnx_57@vnu.edu.vn>
2014-04-10 19:39:57 +0700

    Fix Duplicated code

--- Code smell and refactor/Duplicated_Code.py
+++ Code smell and refactor/Duplicated_Code.py
@@ -1,8 +1,7 @@
 __author__ = 'Nam Xuan'
 
 class Sequence_Number:
-    def Three_King(self):
-        array = [0,2,5,1,4,3,10,6]
+    def Soft(self,array):
         index1 = 0
         while index1<len(array)-1:
             index2 = index1 + 1
@@ -13,22 +12,18 @@ class Sequence_Number:
                     array[index2] = temp
                 index2 = index2 + 1
             index1 = index1 + 1
+        return array
+
+    def Three_King(self):
+        array = [0,2,5,1,4,3,10,6]
+        array = self.Soft(array)
 
         print('Three king: ', array[0], ' ' , array[1] , ' ' , array[2] )
 
 
     def Four_King(self):
          array = [0,2,5,1,4,3,10,6]
-         index1 = 0
-         while index1<len(array)-1:
-            index2 = index1 + 1
-            while index2 < len(array):
-                if array[index1] <array[index2]:
-                    temp = array[index1]
-                    array[index1] = array[index2]
-                    array[index2] = temp
-                index2 = index2 + 1
-            index1 = index1 + 1
+         array = self.Soft(array)
 
          print('Four king: ', array[0], ' ' , array[1] , ' ' , array[2] , ' ' , array[3])
 
