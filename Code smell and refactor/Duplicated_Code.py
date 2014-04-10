598720a8a758458e702ee4baab692cf29a787586
namnx228 <namnx_57@vnu.edu.vn>
2014-04-10 19:38:37 +0700

    Duplicated Code smell

---
+++ Code smell and refactor/Duplicated_Code.py
@@ -0,0 +1,40 @@
+__author__ = 'Nam Xuan'
+
+class Sequence_Number:
+    def Three_King(self):
+        array = [0,2,5,1,4,3,10,6]
+        index1 = 0
+        while index1<len(array)-1:
+            index2 = index1 + 1
+            while index2 < len(array):
+                if array[index1] <array[index2]:
+                    temp = array[index1]
+                    array[index1] = array[index2]
+                    array[index2] = temp
+                index2 = index2 + 1
+            index1 = index1 + 1
+
+        print('Three king: ', array[0], ' ' , array[1] , ' ' , array[2] )
+
+
+    def Four_King(self):
+         array = [0,2,5,1,4,3,10,6]
+         index1 = 0
+         while index1<len(array)-1:
+            index2 = index1 + 1
+            while index2 < len(array):
+                if array[index1] <array[index2]:
+                    temp = array[index1]
+                    array[index1] = array[index2]
+                    array[index2] = temp
+                index2 = index2 + 1
+            index1 = index1 + 1
+
+         print('Four king: ', array[0], ' ' , array[1] , ' ' , array[2] , ' ' , array[3])
+
+
+d = Sequence_Number()
+d.Three_King()
+d.Four_King()
+
+
