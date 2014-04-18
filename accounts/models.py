13be977a8f143e9a8cf5a4036c538f5be8ec5165
namnx228 <namnx_57@vnu.edu.vn>
2014-04-18 08:37:36 +0700

    fix bug in user profile

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User, AbstractBaseUser
 
+
 # Create your models here.
 from django.db.models.fields import IntegerField
 
@@ -10,13 +11,13 @@ class UserProfile(AbstractBaseUser):
     user = models.OneToOneField(User)
 
     # The additional attributes we wish to include.
-    website = models.URLField(blank=True)
+    #website = models.URLField(blank=True)
     # picture = models.ImageField(upload_to='profile_images', blank=True)
 
-    birthday = models.DateField()
-    home = models.CharField(max_length= 200)
-    Score_of_the_last_test = IntegerField()
-    Total_score = IntegerField()
+    birthday = models.DateField(default='22/8/1994')
+    home = models.CharField(default='ha noi',max_length= 200)
+    Score_of_the_last_test = IntegerField(default=10)
+    Total_score = IntegerField(default=10)
     REQUIRED_FIELDS = ['birthday','home','Score of the last test','Total score']
     # Override the __str__() method to return out something meaningful!
     def __str__(self):
