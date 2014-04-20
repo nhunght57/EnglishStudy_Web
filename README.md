3b66486636f80a3dc3d53ceba3ceb8c951b83450
Ngọc Nguyên <k15bhahaha@gmail.com>
2014-04-20 10:29:42 +0700

    Fix some test and update README.md

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
-Studying-English-web
+EnglishStudy_Web
 ====================
 
-our project
+This is a learn-and-made project of the 8th group in K57CA. It is a small website powered by Django framework
+The website has two main features: Multiple-choice test and fill blank missing word in a paragraph, which is heard in a video
+
+To run this project:
+
+ - Install Python 3.x
+ - Install Django 1.7
+ - In the project folder, run `python manage.py runserver <Optional port number>`. Default port number is 8000 if you leave blank at the port number
+     For example `python manage.py runserver 80`
--- tracnghiem/tests.py
+++ tracnghiem/tests.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
 from django.core.urlresolvers import reverse
 
@@ -10,16 +11,37 @@ def create_questions(question_text):
 
 
 class TracnghiemMethodsTest(TestCase):
-    def test_view_with_no_questions(self):
+    # Set up an account to simulate that user is logged in
+    def setUp(self):
+        username = "example"
+        password = "example"
+        user = User.objects.create_user(username=username, password=password)
+        user.save()
+        self.client.login(username=username, password=password)
+
+
+    # While logged in, but there are no questions in database, an appropriate message should be displayed
+    # instead of a blank page
+    def test_view_with_no_questions_logged_in(self):
         response = self.client.get(reverse('tracnghiem:index'))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "Không có gì")
         self.assertQuerysetEqual(response.context['list_of_questions'], [])
 
-    def test_view_with_one_question(self):
+    # While logged in and there is one question in database, it should be displayed
+    def test_view_with_one_question_logged_in(self):
         create_questions("Somewhat")
         response = self.client.get(reverse('tracnghiem:index'))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "Somewhat")
         self.assertNotContains(response, "Không có gì thú vị ở đây cả")
         self.assertQuerysetEqual(response.context['list_of_questions'], ['<Question: Somewhat>'])
+
+    # While logged out, an appropriate message should be displayed instead of content
+    def test_view_logged_out(self):
+        create_questions("Somewhat")
+        self.client.logout()
+        response = self.client.get(reverse('tracnghiem:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Bạn chưa đăng nhập")
+        self.assertNotContains(response, "Somewhat")
