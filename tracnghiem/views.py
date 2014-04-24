6d5e66330899b6c0aceb3d885856e4ab18cb70c8
namnx228 <namnx_57@vnu.edu.vn>
2014-04-25 01:06:10 +0700

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	accounts/tests.py
    	db.sqlite3

--- tracnghiem/views.py
+++ tracnghiem/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views import generic
-from tracnghiem.models import Question
+from tracnghiem.models import Question, Choice
 from django.http import HttpResponse, HttpResponseRedirect
 # Create your views here.
 
@@ -11,9 +11,30 @@ class TracNghiemView(generic.ListView):
     def get_queryset(self):
         return Question.objects.all()
 
+def change_user_score(user, score):
+    user.profile.score_of_the_last_test = score
+    user.profile.total_score += score
+    user.profile.save()
+
 
 def post(request):
-    return HttpResponse("<p>You've just submitted<p>")
+    #return HttpResponse("<p>You've just submitted<p>")
+    QUESTION = 'question'
+    score = 0
+    count_questions = 1
+    question = QUESTION + str(count_questions)
+
+    while question in request.POST:
+        if Choice.objects.get(id=request.POST[question]).is_correct is True:
+            score = score + 1
+        count_questions = count_questions + 1
+        question = QUESTION + str(count_questions)
+
+    change_user_score(request.user,score)
+    return HttpResponse("<p> your score  at this test is " +
+                        str(score) +
+                        " and your total... is "+str(request.user.profile.total_score) + "</p>")
+
 
 
 class ResultView(generic.DetailView):
